086ba9e310a0e7a5d8dc59c14f47271215a9aea2
shao0213 <B10823055@gemail.yuntech.edu.tw>
2023-03-28 15:37:04 +0800

    修正好友人數，刪除計算重複名字，以及版型修正

--- static/js/MsgGoogleSheetAPI.js
+++ static/js/MsgGoogleSheetAPI.js
@@ -9,10 +9,13 @@ const MsgSheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${MsgSheetId}
 fetch(MsgSheetUrl)
 .then(response => response.json())
 .then(data => {
-    document.getElementById("Number_Of_Friends").innerHTML = data.values.length
+    var allTheName = [];
     var tableBody = document.getElementById("MsgTable").getElementsByTagName("tbody")[0];
 
     for (var i = 0; i < data.values.length; i++) {
+        // 把 Name 都放進去，之後要檢查有無重複
+        allTheName.push(data.values[i][1]);
+
         var row = document.createElement("tr");
         var cell1 = document.createElement("td");
         cell1.innerHTML = data.values[i][0];
@@ -28,7 +31,14 @@ fetch(MsgSheetUrl)
 
         tableBody.appendChild(row);
     }
+    UniqueValues(allTheName);
 })
 .catch(error => {
     console.error('Error fetching data:', error);
 });
+
+function UniqueValues(allTheName){
+    var uniqueValues = new Set(allTheName);
+    document.getElementById("Number_Of_Friends").innerHTML = uniqueValues.size;
+    console.log(uniqueValues.size)
+}
